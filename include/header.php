73ee25d629e7fc4b6732dc4504cf75e0a4fd5466
Catalin-code <catalin.despa.code@gmail.com>
2022-12-03 21:56:50 +0200

    Inital commit

---
+++ include/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP-Hospital</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css" integrity="sha384-QYIZto+st3yW+o8+5OHfT6S482Zsvz2WfOzpFSXMF9zqeLcFV0/wlZpMtyFcZALm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.1.slim.js" integrity="sha256-tXm+sa1uzsbFnbXt8GJqsgi2Tw+m4BLGDof6eUPjbtk=" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+include("include/header.php");
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP-Hospital</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-info bg-info">
+        <h5 class="text-white">PHP-Hospital</h5>
+
+        <div class="mr-auto"></div>
+
+        <ul class="">
+            <li class="nav-item"><a href="" class="nav-link">Admin</a></li>
+            <li class="nav-item"><a href="" class="nav-link">Doctor</a></li>
+            <li class="nav-item"><a href="" class="nav-link">Patient</a></li>
+        </ul>
+    </nav>
+    
+</body>
+</html>
