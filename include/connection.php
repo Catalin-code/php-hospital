93dcf71d913079fcd86d0b588baa8d5f1f6d9daa
Catalin-code <catalin.despa.code@gmail.com>
2023-01-18 22:27:38 +0200

    Change database connection values.

--- include/connection.php
+++ include/connection.php
@@ -1,13 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "BalauruMysql99";
-$database = "php_hospital";
+$servername = "eu-cdbr-west-03.cleardb.net";
+$username = "b18405b5b127ed";
+$password = "b3c48de6";
+$database = "heroku_d6f5639d81c6b24";
 
 $connect = mysqli_connect($servername, $username, $password, $database);
-
-// if ($connect->connect_error) {
-//   die("Connection failed: " . $connect->connect_error);
-// }
-// echo "Connected successfully";
 ?>
